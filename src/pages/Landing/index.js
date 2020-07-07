4ada2de1b099231c9690b34bdc9dab92a95fecde
hieudn2012 <51831671+hieudn2012@users.noreply.github.com>
2020-07-07 09:55:35 +0700

    Add UI container

--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -1,17 +1,40 @@
 import React from 'react';
-import Login from '../../components/Login'
+import Login from '../../components/Login';
 
 export default function Landing() {
-  
+
   return (
-    <div className="container-login-page">
-      <div className="row justify-content-center w-100">
-        <div className="col-lg-8 col-md-0">
-        </div>
-        <div className="col-lg-4 col-md-8">
-          <Login />
-        </div>
-      </div>
+    <div>
+      <header>
+        <section></section>
+      </header>
+      <main>
+        <section className="ctn-landing">
+          <div className="container-fluid">
+            <div className="row">
+              <div className="col-md-6">
+                <div className="text-white py-5">
+                  <h4>Ura landing page template</h4>
+                  <p className="py-3">A versatile and easy solution for your all kinds of software and business landing page needs.</p>
+                  <button className="btn btn-danger">KNOW MORE</button>
+                </div>
+              </div>
+              <div className="col-md-6">
+                <Login />
+              </div>
+            </div>
+          </div>
+        </section>
+        <section>
+          <div className="container">
+            <div className="mt-5 py-5 text-center">
+              <h4>Using Ura to build your site is easy</h4>
+              <p>Best in class big data software and analytics services will render</p>
+              <p>your huge chunks into meaningful data. Try the demo now.</p>
+            </div>
+          </div>
+        </section>
+      </main>
     </div>
   )
 }
