e5738fd521ce44117b121c89415acf0ac2d1dd20
hieudn2012 <51831671+hieudn2012@users.noreply.github.com>
2020-07-09 15:34:22 +0700

    Add panel chat room

---
+++ src/components/PanelChat/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function PanelChat() {
+
+  return (
+    <div className="card bg-nav-dark mt-4 wrap-panel-high">
+      <div className="card-body">
+        <h5 className="card-title">Chat Room</h5>
+      </div>
+    </div>
+  )
+}
