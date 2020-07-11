b9938e437cdf94311b9ad338ef6498c01a54732a
hieudn2012 <51831671+hieudn2012@users.noreply.github.com>
2020-07-11 09:10:22 +0700

    Clear box shadow

--- src/components/panelchat/index.js
+++ src/components/panelchat/index.js
@@ -6,7 +6,7 @@ export default function PanelChat() {
   const [show, setShow] = useState(true);
 
   return (
-    <Panel className="mt-4 border border-secondary box-effect" collapse show={show} onCollapse={() => setShow(!show)} textHeader="Chat">
+    <Panel className="mt-4 border border-secondary" collapse show={show} onCollapse={() => setShow(!show)} textHeader="Chat">
       <div>
         <h5 className="card-title">Chat Room</h5>
       </div>
--- src/components/topdata/index.js
+++ src/components/topdata/index.js
@@ -7,28 +7,28 @@ export default function TopData() {
       <h4 className="mt-4">Dashboard</h4>
       <div className="row">
         <div className="col-md-3">
-          <Panel className="mt-2 zoom box-effect">
+          <Panel className="mt-2 zoom">
             <h5 className="card-title text-primary">Total Task</h5>
             <p className="card-text">2993</p>
             <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
           </Panel>
         </div>
         <div className="col-md-3">
-          <Panel className="mt-2 zoom box-effect">
+          <Panel className="mt-2 zoom">
             <h5 className="card-title text-danger">Todo</h5>
             <p className="card-text">1232</p>
             <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
           </Panel>
         </div>
         <div className="col-md-3">
-          <Panel className="mt-2 zoom box-effect">
+          <Panel className="mt-2 zoom">
             <h5 className="card-title text-warning">Inprogress</h5>
             <p className="card-text">23</p>
             <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
           </Panel>
         </div>
         <div className="col-md-3">
-          <Panel className="mt-2 zoom box-effect">
+          <Panel className="mt-2 zoom">
             <h5 className="card-title text-success">Done</h5>
             <p className="card-text">1020</p>
             <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
